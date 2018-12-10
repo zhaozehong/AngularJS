b6878e9ad480593ac43a30435c315dc28b8e69bf
Zehong Zhao <zehong.zhao@HexagonMetrology.com>
2018-12-10 13:15:44 +0800

    pass controller's method to directive

--- AngularJS/SmartFactory/controllers/MainController.js
+++ AngularJS/SmartFactory/controllers/MainController.js
@@ -4,8 +4,7 @@ var MainController = function ($scope, $interval, $location) {
   $scope.charttype = 'line';
   $scope.getNextUrl = function (eventParams) {
     if (eventParams.name) {
-      //return "/factory/" + eventParams.name;
-      return "/machine/" + eventParams.name;
+      return "/factory/" + eventParams.name;
     }
   }
 };
@@ -19,16 +18,13 @@ app.directive('sfchartelement', function ($location) {
       item: "=item11",
       data: "=data11",
       charttype: "=charttype",
-      fixedcharttype: "@"
+      fixedcharttype: "@",
+      next: "&nextUrl"
     },
     restrict: 'E',
     template: '<div style="height: 100%; margin-left: 10px; margin-right: 10px"></div>',
     replace: true,
     link: function ($scope, element, attrs, controller) {
-      element.on("click", function (params) {
-
-      });
-
       var option = sfchart.getEChartOption($scope.fixedcharttype ? $scope.fixedcharttype : $scope.charttype);
       if (option && typeof option === "object") {
         var myChart = echarts.init(document.getElementById($scope.id));
@@ -44,15 +40,14 @@ app.directive('sfchartelement', function ($location) {
               // 点击到了 index 为 5 的 series 的 markPoint 上。
             }
           }
-          else if (params.componentType === 'series' && params.seriesType === 'map') {
-            var nextUrl = $scope.$parent.getNextUrl(params);
+          else if (params.componentType === 'series') {
+            var nextUrl = $scope.next({ params: params});
             if (nextUrl) {
               $location.path(nextUrl);
               window.location = $location.$$absUrl;
             }
           }
         });
-
       }
     }
   };
