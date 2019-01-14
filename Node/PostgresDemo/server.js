4581acfdab21e9eaf456ae277a971cc2b9e298cb
Zehong Zhao <zehong.zhao@HexagonMetrology.com>
2019-01-14 19:00:11 +0800

    create a demo for nodeJS-Postgres connection

---
+++ Node/PostgresDemo/server.js
@@ -0,0 +1,16 @@
+﻿//加载对应的模块
+var http = require("http");
+var url = require("url");
+
+function start(client, route, handle) {
+  //创建http服务器
+  http.createServer(function (request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for '" + pathname + "' received.");
+    route(client, handle, pathname, response);
+  }).listen(8888);   //指定端口
+
+  console.log("Server has started.\n");
+}
+
+exports.start = start;
