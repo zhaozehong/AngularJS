136b591d4cf1516166f131ec0dac327116663009
Zehong Zhao <zehong.zhao@HexagonMetrology.com>
2019-01-14 18:11:45 +0800

    connect to postgres database using nodeJS

---
+++ Node/Postgres/client.js
@@ -0,0 +1,27 @@
+﻿var f = require('./function');
+var pg = require('pg');
+// 连接字符串=“tcp:// 用户名 : 密码 @localhost:5432/ 库名”;
+var conString = 'tcp://postgres:123456@localhost:5432/node';
+var client = new pg.Client(conString);
+
+var value = ['10', 'fillp', 'abc'];
+var insertSQLString = 'insert into teacher values($1,$2,$3)';
+var selectSQLString = 'select * from teacher';
+var updateSQLString = "update teacher set NAME='ipone' where ID='4'";
+var deleteSQLString = "delete from teacher where ID='10'";
+
+client.connect(function (error, results) {
+  if (error) {
+    console.log('ClientConnectionReady Error: ' + error.message);
+    client.end();
+    return;
+  }
+  console.log('Connecting to postgres...');
+  console.log('Connected to postgres automatically.');
+  console.log('connection success...\n');
+
+  f._select(client, selectSQLString);
+  f._insert(client, insertSQLString, value);
+  f._select(client, selectSQLString);
+  f._delete(client, deleteSQLString);
+});
---
+++ Node/Postgres/function.js
@@ -0,0 +1,67 @@
+﻿function _insert(client, insertSQLString, value) {
+  console.log("insert beginning");
+  client.query(insertSQLString, value, function (error, results) {
+    if (error) {
+      console.log("ClientReady Error: " + error.message);
+      client.end();
+      return;
+    }
+    console.log('Inserted: ' + results.rowCount + ' row.');
+    console.log('insert success...\n');
+  });
+  console.log("insert end\n");
+}
+
+function _select(client, selectSQLString) {
+  console.log("select beginning");
+  client.query(selectSQLString, function selectCb(error, results, fields) {
+    console.log("in select callback function");
+    if (error) {
+      console.log('GetData Error: ' + error.message);
+      client.end();
+      return;
+    }
+    if (results.rowCount > 0) {
+      var firstResult;
+      var resultSet = '';
+      for (var i = 0, len = results.rowCount; i < len; i++) {
+        firstResult = results.rows[i];
+        resultSet += 'id:' + firstResult.id + ' name:' + firstResult.name + ' pwd:' + firstResult.pwd + '\n';
+      }
+      console.log(resultSet);
+    }
+    /* 添加功能：使查询结果集返回到客户端并保证此函数的通用性. */
+  });
+  console.log("select end\n");
+}
+
+function _update(client, updateSQLString) {
+  console.log("update beginning");
+  client.query(updateSQLString, function (error, results) {
+    if (error) {
+      console.log("ClientReady Error: " + error.message);
+      client.end();
+      return;
+    }
+    console.log('update success...\n');
+  });
+  console.log("update end\n");
+}
+
+function _delete(client, deleteSQLString) {
+  console.log("delete beginning");
+  client.query(deleteSQLString, function (error, results) {
+    if (error) {
+      console.log("ClientReady Error: " + error.message);
+      client.end();
+      return;
+    }
+    console.log('delete success...\n');
+  });
+  console.log("delete end\n");
+}
+
+exports._insert = _insert;
+exports._select = _select;
+exports._update = _update;
+exports._delete = _delete;
